511eb904fd06c3aa8116e2dd6173550397a62c35
Thomas Bolling <Thomas.Bolling.87@gmail.com>
2021-08-03 00:56:56 -0500

    got chat to display in window still need to get the useMutations to work and rooms to work properly

--- client/src/Pages/Chatroom/Chatroom.js
+++ client/src/Pages/Chatroom/Chatroom.js
@@ -1,38 +1,55 @@
-import React from 'react'
-import {Container, Button, Row, Col, Card} from 'react-bootstrap'
+import React, { useState } from 'react'
+import {Container, Button, Row, Col, Card} from 'react-bootstrap';
+import { useMutation, useQuery, gql } from '@apollo/client';
 import './styles.css'
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { useQuery, gql } from '@apollo/client';
-
-const client = new ApolloClient ({
-    uri: 'http://localhost:3001/',
-    cache: new InMemoryCache()
-});
-
 const GET_MESSAGES = gql`
-query {
-    messages {
-      id
-      content
-      user
+    query {
+        messages {
+            id
+            content
+            user
+        }
     }
-  }
+`;
+
+const POST_MESSAGE = gql`
+    mutation($user: String!, $content: String!) {
+        postMessage(user: $user, content: $content)
+    }
 `;
 
 const Messages = ({ user }) => {
     const { data } = useQuery(GET_MESSAGES);
     if (!data) {
         return null;
-    }
-
-    return JSON.stringify(data);
+    } 
+    return (
+        <>
+            {data.messages.map(({ id, user: messageUser, content }) => (
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+                        padding: "1rem"
+                    }}
+                >
+                    <div
+                        style={{
+                            background: user === messageUser ? "#58bf56" : "#e5e6ea",
+                            color: user === messageUser ? "black" : "blue",
+                            padding: "1rem"
+                        }}
+                    >
+                        {content}
+                    </div> 
+                </div>
+            ))}
+        </>
+    );
 }
 
 const Chatroom = () => {
-
-    <ApolloProvider client={client} />
-
     const itemNameStyle = {
         fontFamily: "Bangers"
     }
@@ -58,9 +75,7 @@ const Chatroom = () => {
                 <div className="timer-two"> Time left until auction ends:
                     <span id="bid-timer" style={auctionStyle}>00:00:00</span>
                 </div>
-                <div>
-                    <Messages user="Thomas" />
-                </div>
+                {/* <div><Messages user="Thomas" /></div> */}
             </div>
 
             <Container className="chatroomContain">
@@ -104,28 +119,41 @@ const Chatroom = () => {
                                         <Card.Img src="" className="rounded-circle" style={imgStyle}/>
                                     </div>
                                     <Container className="chatContainer">
-                                        <div id="test"></div>
+                                        <Messages user="Thomas" />
                                     </Container>
                                 </div>
                             </Card.Body>
                             
 
-                            <Card.Footer className="card-footer cardEnd">
+                            <Card.Footer className="card-footer cardEnd">                              
+                                <div className="chat-form-container">
+                                    <form className="chat-form d-flex msgBody scroll">
+                                        <input 
+                                            className="chat-input"
+                                            type="text"
+                                            placeholder="Enter Message"
+                                            required
+                                            autoComplete="off"
+                                        />
+                                        <Button className="btn chatBtn"><i className="fas fa-paper-plane"></i>Send</Button>
+                                    </form>
+                                </div>
+
                                 <div className="bidDescript text-center" style={itemNameStyle}>
                                     <h5>To start bidding, click the amount you wish to drop it</h5>
                                 </div>
                                 
                                 <div className="d-flex justify-content-center" id="bitBtns">
-                                    <button className="btn bidBtns" type="submit">100</button>
-                                    <button className="btn bidBtns" type="submit">200</button>
-                                    <button className="btn bidBtns" type="submit">300</button>
-                                    <button className="btn bidBtns" type="submit">400</button>
-                                    <button className="btn bidBtns" type="submit">500</button>
-                                    <button className="btn bidBtns" type="submit">1000</button>
+                                    <Button className="btn bidBtns" type="submit">100</Button>
+                                    <Button className="btn bidBtns" type="submit">200</Button>
+                                    <Button className="btn bidBtns" type="submit">300</Button>
+                                    <Button className="btn bidBtns" type="submit">400</Button>
+                                    <Button className="btn bidBtns" type="submit">500</Button>
+                                    <Button className="btn bidBtns" type="submit">1000</Button>
                                     <div className="dropdown" data-toggle="dropdown">
-                                        <button className=" btn customBidBtn" type="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <Button className=" btn customBidBtn" type="Button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                             Custom Bid
-                                        </button>
+                                        </Button>
                                         <div className="dropdown-menu">
                                             <form className="customBid-form">
                                                 <div className="form-group">
@@ -133,7 +161,7 @@ const Chatroom = () => {
                                                     <input className="form-control" type="text" id="bid-form" />
                                                 </div>
                                                 <div className="form-group">
-                                                    <button className="customBidBtn" id="customBidBtn" type="submit">Enter</button>
+                                                    <Button className="customBidBtn" id="customBidBtn" type="submit">Enter</Button>
                                                 </div>
                                             </form>
                                         </div>
@@ -147,5 +175,6 @@ const Chatroom = () => {
         </>
     )
 }
+
 
 export default Chatroom
