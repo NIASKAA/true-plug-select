fbc836f8cf02fec4a898f23deba2fac14bacddcf
NIASKA <mkman751@yahoo.com>
2021-08-07 10:14:29 -0500

    basic searchbar setup

--- client/src/Pages/Bids/Bids.js
+++ client/src/Pages/Bids/Bids.js
@@ -1,35 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import ProductCard from "../../Components/ProductCard/ProductCard"
 import { Get_All_Products, Query_User } from "../../utils/queries";
 import { GET_ALL_PRODUCTS } from "../../utils/state/actions";
 import { useDispatch, useSelector } from "react-redux";
 import {Container, Button, Row, InputGroup, FormControl} from 'react-bootstrap'
 import ProductList from '../../Components/ProductList/ProductList';
+import ProductCard from '../../Components/ProductCard/ProductCard';
 import Auth from '../../utils/auth';
 import './styles.css'
 
-const Bids = () => {
-  let history = useHistory();
-  const auctionDirect = () => {
-    history.push("/auctionform");
-  };
-  const redirect = () => {
-    history.push("/login");
-  };
-
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
-  const { loading, data } = useQuery(Get_All_Products);
-  const { auctions } = state;
-  useEffect(() => {
-    if (loading == false && data) {
-      dispatch({ type: GET_ALL_PRODUCTS, payload: data });
-      console.log(state);
-    }
-  }, [loading, data]);
-
+const Bids = ({products}) => {
+    let history = useHistory();
+    const auctionDirect = () => {
+        history.push("/auctionform");
+    };
+    const redirect = () => {
+        history.push("/login");
+    };
+    
     const dispatch = useDispatch();
     const state = useSelector((state) => state);
     const { loading, data } = useQuery(Get_All_Products);
@@ -40,20 +29,20 @@ const Bids = () => {
       }
     }, [loading, data]);
     const [index, setIndex] = useState(0);
-    const [searchItem, setSearchItem] = useState("")
+    const [searchItem, setSearchItem] = useState("");
     const handleSelect = (selectedIndex, e) => {
       setIndex(selectedIndex);
     };
 
-    /*<Row>
-        {products.filter((product) => {
-            if(searchItem = "") {
-                return product
-            } else if (product.itemName.toLowerCase().includes(searchItem.toLowerCase())) {
-                return <ProductCard product={product} key={product.id} />
-            }
-        })}
-    </Row>*/
+    /* {products.filter((product) => {
+        if(searchItem == "") {
+            return product
+        } else if(products.itemName.toLowerCase().includes(searchItem.toLowerCase())) {
+            return <ProductCard product={product} key={product.id} />
+        }
+    })} */
+    
+    console.log(data)
     return (
         <>
             <Container className="container text-center my-3">
@@ -69,9 +58,7 @@ const Bids = () => {
                         placeholder="Search an item"
                         aria-label="searchbar"
                         aria-describedby="searchbar"
-                        onChange={(event) => {
-                            setSearchItem(event.target.value)
-                        }}
+                        onChange={(event)=> {setSearchItem(event.target.value)}}
                     />
                 </InputGroup>
                 
