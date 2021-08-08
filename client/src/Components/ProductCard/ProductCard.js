4d085554f996ecb1d7d295428305a5d7aa2cdd07
NIASKA <mkman751@yahoo.com>
2021-08-08 15:47:46 -0500

    added brands to auctionform

--- client/src/Components/ProductCard/ProductCard.js
+++ client/src/Components/ProductCard/ProductCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {Link, useParams} from 'react-router-dom'
 import { Card, Button, Col} from "react-bootstrap";
 import './styles.css'
