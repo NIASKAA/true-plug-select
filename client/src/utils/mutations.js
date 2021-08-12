1f6655b00080813e80b4bd90b643db85971f82b3
Santos <max.go.95@gmail.com>
2021-08-12 11:54:52 -0500

    Added win auction functionality, the chatroom now hides the bid buttons if the auction ends

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -117,14 +117,9 @@ export const Delete_Product = gql`
 `
 
 export const Win_Auction = gql`
-  mutation wonAuction($auctionId: ID!){
-    auctionId(auctionId: $auctionId) {
+  mutation WinAuction($auctionId: ID!){
+    winAuction(auctionId: $auctionId) {
       bidAmount
-      timeCreated
-      bidder {
-        _id
-        username
-      }
     }
   }`
 
